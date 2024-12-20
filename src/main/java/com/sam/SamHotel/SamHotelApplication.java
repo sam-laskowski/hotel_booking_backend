b1e709c43643226d776f3e53a98e2c2a8acbc30d
samelpo <sam.laskowski@outlook.com>
2024-12-20 17:24:32 +0000

    first commit

---
+++ src/main/java/com/sam/SamHotel/SamHotelApplication.java
@@ -0,0 +1,13 @@
+package com.sam.SamHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamHotelApplication.class, args);
+	}
+
+}
