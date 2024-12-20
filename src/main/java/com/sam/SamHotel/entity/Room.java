8db2ce1b9ac8679860f63642a0e6d6e96ddb72e8
samelpo <sam.laskowski@outlook.com>
2024-12-20 19:02:29 +0000

    added ourexception and fixed room repository query

--- src/main/java/com/sam/SamHotel/entity/Room.java
+++ src/main/java/com/sam/SamHotel/entity/Room.java
@@ -28,6 +28,7 @@ public class Room {
     private BigDecimal roomPrice;
     private String roomPhotoUrl;
     private String roomDescription;
+    
     @OneToMany(mappedBy = "room", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Booking> bookings = new ArrayList<>();
 
---
+++ src/main/java/com/sam/SamHotel/exception/OurException.java
@@ -0,0 +1,7 @@
+package com.sam.SamHotel.exception;
+
+public class OurException extends RuntimeException {
+    public OurException(String message) {
+        super(message);
+    }
+}
