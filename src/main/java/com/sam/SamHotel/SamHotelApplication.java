f36ab21663ade37d2d4260b88eef22c9053e2de3
samelpo <sam.laskowski@outlook.com>
2024-12-20 21:54:53 +0000

    added aws keys

--- src/main/java/com/sam/SamHotel/SamHotelApplication.java
+++ src/main/java/com/sam/SamHotel/SamHotelApplication.java
@@ -3,10 +3,12 @@ package com.sam.SamHotel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SamHotelApplication {
-
 	public static void main(String[] args) {
+		Dotenv.configure().load();
 		SpringApplication.run(SamHotelApplication.class, args);
 	}
 
