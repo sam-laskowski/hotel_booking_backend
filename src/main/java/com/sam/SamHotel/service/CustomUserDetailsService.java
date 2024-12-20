c4e08d8338f6794dea31c240c925e1f75535e0a2
samelpo <sam.laskowski@outlook.com>
2024-12-20 21:55:09 +0000

    added jwt

---
+++ src/main/java/com/sam/SamHotel/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.sam.SamHotel.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.sam.SamHotel.exception.OurException;
+import com.sam.SamHotel.repo.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new OurException("Username/Email not found"));
+    }
+}
