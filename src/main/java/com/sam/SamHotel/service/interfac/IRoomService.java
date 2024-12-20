fec05c04a805c7e61baafc65d898c6651eb843db
samelpo <sam.laskowski@outlook.com>
2024-12-20 23:20:59 +0000

    created room service

---
+++ src/main/java/com/sam/SamHotel/service/interfac/IRoomService.java
@@ -0,0 +1,27 @@
+package com.sam.SamHotel.service.interfac;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sam.SamHotel.dto.Response;
+
+public interface IRoomService {
+    Response addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice, String description);
+
+    List<String> getAllRoomTypes();
+
+    Response getAllRooms();
+
+    Response deleteRoom(Long roomId);
+
+    Response updateRoom(Long roomId, String description, String roomType, BigDecimal roomPrice, MultipartFile photo);
+
+    Response getRoomById(Long roomId);
+
+    Response getAvailableRoomsByDataAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
+
+    Response getAllAvailableRooms();
+}
